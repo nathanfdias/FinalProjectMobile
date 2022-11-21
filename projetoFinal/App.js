0ca4240df822fd88f46a7acf0e3ab394a1b6e06f
RivaniaGomes <rivaniagomes@gmail.com>
2022-11-21 20:06:42 -0300

    alteração texto welcome

--- projetoFinal/App.js
+++ projetoFinal/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes';
--- projetoFinal/src/pages/Welcome/index.js
+++ projetoFinal/src/pages/Welcome/index.js
@@ -1,73 +1,88 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-import * as Animatable from 'react-native-animatable';
+import * as Animatable from "react-native-animatable";
 
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 
 export default function Welcome() {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    return(
-        <View style={styles.container}>
-            <View style={styles.logo}>
-                <Animatable.Image animation='flipInX' delay={100} source={require('../../assets/logo.png')} style={styles.logoImg}/>
-            </View>
-            <Animatable.View delay={700} animation='fadeInUp' style={styles.footer}>
-                <Text style={styles.title}>Os melhores veículos você encontra na AsphaltCoding</Text>
-                <Text style={styles.txt}>Faça o login para começar</Text>
-
-                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
-                    <Text style={styles.buttonText}>Acessar</Text>
-                </TouchableOpacity>
-            </Animatable.View>
-        </View>
-    )
-}  
+  return (
+    <View style={styles.container}>
+      <View style={styles.logo}>
+        <Animatable.Image
+          animation='flipInX'
+          delay={100}
+          source={require("../../assets/logo.png")}
+          style={styles.logoImg}
+        />
+      </View>
+      <Animatable.View
+        delay={700}
+        animation='fadeInUp'
+        style={styles.footer}>
+        <Text style={styles.title}>
+          Os melhores veículos você encontra na AsphaltCoding
+        </Text>
+        <Text style={styles.txt}>Faça o login para começar</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Login")}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+      </Animatable.View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: '#7B0000',
-    },
-    logo:{
-        flex: 2,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    logoImg:{
-        width: 100,
-        height: 100,
-    },
-    footer:{
-        flex: 1,
-        backgroundColor: '#FFF',
-        borderTopRightRadius: 25,
-        borderTopLeftRadius: 25,
-        paddingStart: '5%',
-        paddingEnd: '5%',
-    },
-    title:{
-        fontSize: '24%',
-        fontWeight: 'bold',
-        marginTop: 28,
-        marginBottom: 18,
-    },
-    button:{
-        position: 'absolute',
-        backgroundColor: '#7B0000',
-        borderRadius: 50,
-        paddingVertical: '5%',
-        width: '60%',
-        alignSelf: 'center',
-        bottom: '15%', 
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    buttonText:{
-        color: '#FFF',
-        fontSize: 14,
-        fontWeight: 'bold',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#7B0000",
+  },
+  logo: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoImg: {
+    width: 100,
+    height: 100,
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    paddingStart: "5%",
+    paddingEnd: "5%",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 28,
+    marginBottom: 18,
+  },
+  txt: {
+    color: "#696969",
+    fontSize: 16,
 
-})
+  },
+  button: {
+    position: "absolute",
+    backgroundColor: "#7B0000",
+    borderRadius: 50,
+    paddingVertical: "5%",
+    width: "60%",
+    alignSelf: "center",
+    bottom: "15%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
