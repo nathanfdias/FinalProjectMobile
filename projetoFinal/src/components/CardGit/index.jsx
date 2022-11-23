759fe7359575eb6c21d811d4be109c5024e99fe2
Leandro Ferraz <85909017+FerrazLeandro@users.noreply.github.com>
2022-11-23 15:40:42 -0300

    Implementação do sobre

--- projetoFinal/src/components/CardGit/index.jsx
+++ projetoFinal/src/components/CardGit/index.jsx
@@ -1,24 +1,24 @@
 import { useNavigation } from "@react-navigation/native"
-import { Text, TouchableOpacity, View, StyleSheet, Image, Linking, ScrollView } from "react-native"
+import { Text, TouchableOpacity, View, StyleSheet, Image, Linking } from "react-native"
 
 
 const CardGit = ({ item }) => {
     const navigation = useNavigation();
     return (
         <TouchableOpacity   onPress={() => Linking.openURL(item.html_url)}>
-            <View style={styles.container}>
-                <View style={styles.card}>
-                    <Image source={{uri: item.avatar_url}} style={styles.foto}/>
-                    <Text style={styles.textPageTitulo}>{item.name}</Text>
-                </View>
-                <ScrollView style={styles.detalhes}>
-                    <Text style={styles.textPage}>{item.location}</Text>
-                    <Text style={styles.textPage}>{item.company}</Text>
-                    <Text style={styles.textPage}>{item.blog}</Text>
-                    <Text style={styles.textPage}>{item.bio}</Text>
-                    <Text style={styles.textPage}>Seguidores: {item.followers}</Text>
-                </ScrollView>
+        <View style={styles.container}>
+            <View style={styles.card}>
+                <Image source={{uri: item.avatar_url}} style={styles.foto}/>
+                <Text style={styles.textPageTitulo}>{item.name}</Text>
             </View>
+            <View style={styles.detalhes}>
+                <Text style={styles.textPage}>{item.location}</Text>
+                <Text style={styles.textPage}>{item.company}</Text>
+                <Text style={styles.textPage}>{item.blog}</Text>
+                <Text style={styles.textPage}>{item.bio}</Text>
+                <Text style={styles.textPage}>Seguidores: {item.followers}</Text>
+            </View>
+        </View>
         </TouchableOpacity>
             
     )
@@ -26,48 +26,45 @@ const CardGit = ({ item }) => {
 
 const styles = StyleSheet.create({
 container: {
-    margin: 10,
-    width: 170,
-    backgroundColor: 'black',
-    alignItems: 'center',
+    alignItems: 'center', 
+    marginTop: 50,
+    marginBottom: 50
 },
 card: {
     alignItems: 'center',
     paddingTop: 20,
     paddingBottom: 20,
     backgroundColor: '#fff',
-    width: '100%',
+    width: '80%',
     shadowColor: '#171717',
     shadowOpacity: 0.6,
     shadowRadius: 6,
 },
 detalhes: {
-    height: 120,
-    alignSelf: 'center',
-    paddingTop: 5,
-    paddingBottom: 5,
-    marginHorizontal: 4,
+    alignItems: 'center',
+    paddingTop: 30,
+    paddingBottom: 20,
     backgroundColor: '#003580',
-    width: '100%',
-    // borderBottomRightRadius: 90,
+    width: '80%',
+    borderBottomRightRadius: 90,
     shadowColor: '#171717',
     shadowOpacity: 0.6,
     shadowRadius: 6,
 },
 foto: {
-    width: 100,
-    height:100,
+    width: 200,
+    height:200,
     borderRadius:50,
     marginBottom: 20
 },
 textPageTitulo: {
-    fontSize: 14,
+    fontSize: 18,
     color: '#000',
 },
 textPage:{
-    fontSize: 12,
+    fontSize: 18,
     color: '#fff',
 },
 })
 
-export default CardGit;
+export default CardGit
